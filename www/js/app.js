ea817637f12c2bd27575fcaec5174b86bb54b33d
Tabares <jose.tabares.sotelo@accenture.com>
2016-09-28 17:18:42 -0500

    Added checked function

--- www/js/app.js
+++ www/js/app.js
@@ -9,6 +9,20 @@ angular.module('starter', ['ionic', 'leaflet-directive', 'ngCordova', 'igTruncat
     $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
+      var onShake = function() {
+        alert("device is being shaken");
+        //$scope.locate();
+      };
+      var onError = function() {
+        // Fired when there is an accelerometer error (optional)
+          alert("Error on device");
+      };
+
+      // Start watching for shake gestures and call "onShake"
+      // with a shake sensitivity of 40 (optional, default 30)
+      shake.startWatch(onShake, 40 /*, onError */ );
+      // Stop watching for shake gestures
+      //shake.stopWatch();
       if(window.cordova && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         window.cordova.plugins.Keyboard.disableScroll(true);
--- www/js/controllers/mapController.js
+++ www/js/controllers/mapController.js
@@ -1,118 +1,114 @@
-angular.module('starter').controller('MapController',
-  [ '$scope',
-    '$cordovaGeolocation',
-    '$stateParams',
-    '$ionicModal',
-    '$ionicPopup',
-    'LocationsService',
-    'InstructionsService',
-    '$cordovaCamera',
-    '$cordovaCapture',
-    function(
-      $scope,
-      $cordovaGeolocation,
-      $stateParams,
-      $ionicModal,
-      $ionicPopup,
-      LocationsService,
-      InstructionsService,
-      $cordovaCamera,
-      $cordovaCapture
-      ) {
-      /**
-       * Once state loaded, get put map on scope.
-       */
-      $scope.$on("$stateChangeSuccess", function() {
+angular.module('starter').controller('MapController', ['$scope',
+  '$cordovaGeolocation',
+  '$stateParams',
+  '$ionicModal',
+  '$ionicPopup',
+  'LocationsService',
+  'InstructionsService',
+  '$cordovaCamera',
+  '$cordovaCapture',
+  function(
+    $scope,
+    $cordovaGeolocation,
+    $stateParams,
+    $ionicModal,
+    $ionicPopup,
+    LocationsService,
+    InstructionsService,
+    $cordovaCamera,
+    $cordovaCapture
+  ) {
+    /**
+     * Once state loaded, get put map on scope.
+     */
+    $scope.$on("$stateChangeSuccess", function() {
 
-        $scope.locations = LocationsService.savedLocations;
-        $scope.newLocation;
+      $scope.locations = LocationsService.savedLocations;
+      $scope.newLocation;
 
-        if(!InstructionsService.instructions.newLocations.seen) {
+      if (!InstructionsService.instructions.newLocations.seen) {
 
-          var instructionsPopup = $ionicPopup.alert({
-            title: 'Find Me!!!',
-            cssClass: 'front-screen',
-            template: InstructionsService.instructions.newLocations.text
-          });
-          instructionsPopup.then(function(res) {
-            InstructionsService.instructions.newLocations.seen = true;
-            });
+        var instructionsPopup = $ionicPopup.alert({
+          title: 'Find Me!!!',
+          cssClass: 'front-screen',
+          template: InstructionsService.instructions.newLocations.text
+        });
+        instructionsPopup.then(function(res) {
+          InstructionsService.instructions.newLocations.seen = true;
+        });
 
-        }
+      }
 
-        $scope.map = {
-          defaults: {
-            tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-            maxZoom: 18,
-            zoomControlPosition: 'topright'
-          },
-          markers : {},
-          events: {
-            map: {
-              enable: ['context'],
-              logic: 'emit'
-            }
+      $scope.map = {
+        defaults: {
+          tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+          maxZoom: 18,
+          zoomControlPosition: 'topright'
+        },
+        markers: {},
+        events: {
+          map: {
+            enable: ['context'],
+            logic: 'emit'
           }
-        };
+        }
+      };
 
-        $scope.goTo(0);
+      $scope.goTo(0);
 
+    });
+
+    $scope.shake2 = function() {
+      alert('dfdfsf')
+    };
+
+
+  
+
+
+    //CameraController
+    $scope.takePhoto = function() {
+      var options = {
+        quality: 100,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: true,
+        correctOrientation: false
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        $scope.imgURI = "data:image/jpeg;base64," + imageData;
+      }, function(err) {
+        // An error occured. Show a message to the user
+      });
+    }
+
+    $scope.choosePhoto = function() {
+      var options = {
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA,
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageURI) {
+        var image = document.getElementById('myImage');
+        image.src = imageURI;
+        $scope.imgURI = imageURI;
+      }, function(err) {
+        // error
       });
 
-      $scope.shake2 = function(){
-          alert('dfdfsf')
+    }
+
+    $scope.captureAudio = function() {
+      var options = {
+        limit: 3,
+        duration: 10
       };
-      //shake
-      var onShake = function () {
-        // Fired when a shake is detected
-        alert('Shaked phone');
-      };
-
-      var onError = function () {
-        // Fired when there is an accelerometer error (optional)
-        alert('accelerometer error');
-      };
-
-      //CameraController
-      $scope.takePhoto = function() {
-        var options = {
-          quality: 100,
-          destinationType: Camera.DestinationType.DATA_URL,
-          sourceType: Camera.PictureSourceType.CAMERA,
-          allowEdit: false,
-          encodingType: Camera.EncodingType.JPEG,
-          targetWidth: 300,
-          targetHeight: 300,
-          popoverOptions: CameraPopoverOptions,
-          saveToPhotoAlbum: true,
-          correctOrientation: false
-        };
-
-        $cordovaCamera.getPicture(options).then(function(imageData) {
-          $scope.imgURI = "data:image/jpeg;base64," + imageData;
-        }, function(err) {
-          // An error occured. Show a message to the user
-        });
-      }
-
-      $scope.choosePhoto = function() {
-        var options = {
-          destinationType: Camera.DestinationType.FILE_URI,
-          sourceType: Camera.PictureSourceType.CAMERA,
-        };
-
-        $cordovaCamera.getPicture(options).then(function(imageURI) {
-          var image = document.getElementById('myImage');
-          image.src = imageURI;
-          $scope.imgURI = imageURI;
-        }, function(err) {
-        // error
-        });
-
-      }
-
-      $scope.captureAudio = function() {
-      var options = { limit: 3, duration: 10 };
 
       $cordovaCapture.captureAudio(options).then(function(audioData) {
         // Success! Audio data is here
@@ -123,7 +119,9 @@ angular.module('starter').controller('MapController',
     }
 
     $scope.captureImage = function() {
-      var options = { limit: 3 };
+      var options = {
+        limit: 3
+      };
 
       $cordovaCapture.captureImage(options).then(function(imageData) {
         // Success! Image data is here
@@ -134,7 +132,10 @@ angular.module('starter').controller('MapController',
     }
 
     $scope.captureVideo = function() {
-      var options = { limit: 3, duration: 15 };
+      var options = {
+        limit: 3,
+        duration: 15
+      };
 
       $cordovaCapture.captureVideo(options).then(function(videoData) {
         // Success! Video data is here
@@ -144,128 +145,129 @@ angular.module('starter').controller('MapController',
       });
     }
 
-      //CameraController
+    //CameraController
 
-      $scope.cancel = function(){
-        var instructionsPopup = $ionicPopup.alert({
-          title: 'Find Me!!!',
-          cssClass: 'front-screen',
-          template: InstructionsService.instructions.newLocations.text
-        });
-        $scope.map = {
-          defaults: {
-            tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-            maxZoom: 18,
-            zoomControlPosition: 'topright'
-          },
-          markers : {},
-          events: {
-            map: {
-              enable: ['context'],
-              logic: 'emit'
-            }
-          }
-        };
-
-        $scope.goTo(0);
-      }
-
-      var Location = function() {
-        if ( !(this instanceof Location) ) return new Location();
-        this.lat  = "";
-        this.lng  = "";
-        this.name = "";
-      };
-
-      $ionicModal.fromTemplateUrl('templates/addLocation.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function(modal) {
-          $scope.modal = modal;
-        });
-
-      /**
-       * Detect user long-pressing on map to add new location
-       */
-      $scope.$on('leafletDirectiveMap.contextmenu', function(event, locationEvent){
-        $scope.newLocation = new Location();
-        $scope.newLocation.lat = locationEvent.leafletEvent.latlng.lat;
-        $scope.newLocation.lng = locationEvent.leafletEvent.latlng.lng;
-        $scope.modal.show();
+    $scope.cancel = function() {
+      var instructionsPopup = $ionicPopup.alert({
+        title: 'Find Me!!!',
+        cssClass: 'front-screen',
+        template: InstructionsService.instructions.newLocations.text
       });
-
-      $scope.loadForm = function(){
-        $cordovaGeolocation
-          .getCurrentPosition()
-          .then(function (position) {
-            $scope.locate();
-            $scope.newLocation = new Location();
-            $scope.newLocation.lat = position.coords.latitude;
-            $scope.newLocation.lng = position.coords.longitude;
-          }, function(err) {
-            // error
-            console.log("Location error!");
-            console.log(err);
-          });
-        $scope.modal.show();
-      }
-
-      $scope.saveLocation = function() {
-        LocationsService.savedLocations.push($scope.newLocation);
-        $scope.modal.hide();
-        $scope.goTo(LocationsService.savedLocations.length - 1);
+      $scope.map = {
+        defaults: {
+          tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+          maxZoom: 18,
+          zoomControlPosition: 'topright'
+        },
+        markers: {},
+        events: {
+          map: {
+            enable: ['context'],
+            logic: 'emit'
+          }
+        }
       };
 
-      /**
-       * Center map on specific saved location
-       * @param locationKey
-       */
-      $scope.goTo = function(locationKey) {
+      $scope.goTo(0);
+    }
 
-        var location = LocationsService.savedLocations[locationKey];
+    var Location = function() {
+      if (!(this instanceof Location)) return new Location();
+      this.lat = "";
+      this.lng = "";
+      this.name = "";
+    };
 
-        $scope.map.center  = {
-          lat : location.lat,
-          lng : location.lng,
-          zoom : 12
-        };
+    $ionicModal.fromTemplateUrl('templates/addLocation.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
 
-        $scope.map.markers[locationKey] = {
-          lat:location.lat,
-          lng:location.lng,
-          message: location.name,
-          focus: true,
-          draggable: false
-        };
+    /**
+     * Detect user long-pressing on map to add new location
+     */
+    $scope.$on('leafletDirectiveMap.contextmenu', function(event, locationEvent) {
+      $scope.newLocation = new Location();
+      $scope.newLocation.lat = locationEvent.leafletEvent.latlng.lat;
+      $scope.newLocation.lng = locationEvent.leafletEvent.latlng.lng;
+      $scope.modal.show();
+    });
 
+    $scope.loadForm = function() {
+      $cordovaGeolocation
+        .getCurrentPosition()
+        .then(function(position) {
+          $scope.locate();
+          $scope.newLocation = new Location();
+          $scope.newLocation.lat = position.coords.latitude;
+          $scope.newLocation.lng = position.coords.longitude;
+        }, function(err) {
+          // error
+          console.log("Location error!");
+          console.log(err);
+        });
+      $scope.modal.show();
+    }
+
+    $scope.saveLocation = function() {
+      LocationsService.savedLocations.push($scope.newLocation);
+      $scope.modal.hide();
+      $scope.goTo(LocationsService.savedLocations.length - 1);
+    };
+
+    /**
+     * Center map on specific saved location
+     * @param locationKey
+     */
+    $scope.goTo = function(locationKey) {
+
+      var location = LocationsService.savedLocations[locationKey];
+
+      $scope.map.center = {
+        lat: location.lat,
+        lng: location.lng,
+        zoom: 12
       };
 
-      /**
-       * Center map on user's current position
-       */
-      $scope.locate = function(){
-
-        $cordovaGeolocation
-          .getCurrentPosition()
-          .then(function (position) {
-            $scope.map.center.lat  = position.coords.latitude;
-            $scope.map.center.lng = position.coords.longitude;
-            $scope.map.center.zoom = 18;
-
-            $scope.map.markers.now = {
-              lat:position.coords.latitude,
-              lng:position.coords.longitude,
-              message: "You Are Here",
-              focus: true,
-              draggable: false
-            };
-
-          }, function(err) {
-            // error
-            console.log("Location error!");
-            console.log(err);
-          });
-
+      $scope.map.markers[locationKey] = {
+        lat: location.lat,
+        lng: location.lng,
+        message: location.name,
+        focus: true,
+        draggable: false
       };
 
-    }]);
+    };
+
+    /**
+     * Center map on user's current position
+     */
+    $scope.locate = function() {
+
+      $cordovaGeolocation
+        .getCurrentPosition()
+        .then(function(position) {
+          $scope.map.center.lat = position.coords.latitude;
+          $scope.map.center.lng = position.coords.longitude;
+          $scope.map.center.zoom = 18;
+
+          $scope.map.markers.now = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+            message: "You Are Here",
+            focus: true,
+            draggable: false
+          };
+
+        }, function(err) {
+          // error
+          console.log("Location error!");
+          console.log(err);
+        });
+
+    };
+
+  }
+]);
